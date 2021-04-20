7b95baa7bf8b7b3d2880fe82cc84625be6b8346b
奕淳 王 <forever2635@yahoo.com>
2021-04-20 16:01:58 +0800

    第一題final

--- 109_2Mid/Sample1Com.aspx.cs
+++ 109_2Mid/Sample1Com.aspx.cs
@@ -8,7 +8,7 @@ using System.Web.UI.WebControls;
 namespace _109_2Mid {
     public partial class Sample1Com : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
-            lb_Res.Text = Request.Form.Get("tb_Acc") + "</br>" + Request.Form.Get("tb_Pass") + "</br>" +Request.Form.Get("");
+            lb_Res.Text = Request.Form.Get("tb_Acc") + "</br>" + Request.Form.Get("tb_Pass") + "</br>" ;
 
             if (Request.Form.Get("rbg_Interest") == "rb1")
             {
